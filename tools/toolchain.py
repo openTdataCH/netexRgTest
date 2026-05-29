93bcd4195e9aa09a75270cc992a884cce262bfea
Urs Stöckli <urs.stoeckli@sbb.ch>
2026-05-29 17:23:50 +0200

    Remove arg parser.

--- tools/toolchain.py
+++ tools/toolchain.py
@@ -6,14 +6,17 @@ from tools.expand_docs.expand_docs import expand_docs
 from tools.md2html.md2html import generate_html_files
 from tools.md_builder.md_builder import build_markdown_tables
 
-def main():
-    parser = argparse.ArgumentParser(description='Run tools to generate documents.')
-    parser.add_argument('--target', default="docs", help=f"Target to generate (default: docs)")
-    args = parser.parse_args()
+def generate_docs():
     expand_docs(DOCS_DIR, GENERATED_DOCS_DIR)
     generate_html_files(GENERATED_DOCS_DIR)
+
+def generate_tables():
     build_markdown_tables(TEMPLATES_DIR, GENERATED_TABLES_DIR, XSD_FILE_PATH)
     generate_html_files(GENERATED_TABLES_DIR)
+
+def main():
+    generate_docs()
+    generate_tables()
 
 if __name__ == '__main__':
     main()
